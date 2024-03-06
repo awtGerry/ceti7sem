9cbbed3699143e46527d536e4baac72de82e3b08
Victor Rodriguez <awtgerry@gmail.com>
2024-03-06 11:41:43 -0600

    practicas

--- graphics-java/1p/runner.sh
+++ graphics-java/1p/runner.sh
@@ -6,12 +6,18 @@ case $1 in
     "4") file="OrdNumber.java" ;;
     "5") file="Substring.java" ;;
     "6") file="Hex2IP.java" ;;
+    "7") file="Ventana.java" ;;
+    "8") file="Arquimedes.java" ;;
+    "9") file="Pastel.java" ;;
+    "10") file="Monito.java" ;;
+    "11") file="Visor.java" ;;
+    "12") file="Mouse.java" ;;
     *)
         echo "No hay programa para el número $1."
         exit 1
         ;;
 esac
 
-echo "Compiling $file..."
+echo "Running $file..."
 echo ""
 java --source 21 --enable-preview $file
