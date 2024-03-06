beff968708b20f15863b11b83b4e2c3b1a94b9e2
Victor Rodriguez <awtgerry@gmail.com>
2024-03-06 10:43:37 -0600

    Arquimedes fix

--- graphics-java/1p/Arquimedes.java
+++ graphics-java/1p/Arquimedes.java
@@ -1,14 +1,16 @@
-/* Draw archimedean spiral using threads to draw it slowly */
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 
 class Arquimedes extends JFrame implements Runnable {
+
+    static final int WIDTH = 800;
+    static final int HEIGHT = 600;
+
     public Arquimedes() {
         setTitle("Arquimedes");
-        setSize(800, 600);
+        setSize(WIDTH, HEIGHT);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
@@ -17,18 +19,28 @@ class Arquimedes extends JFrame implements Runnable {
     public void paint(Graphics g) {
         super.paint(g);
         g.setColor(Color.RED);
-        int x = 400;
-        int y = 300;
-        int r = 0;
-        for (int i = 0; i < 1000; i++) {
-            r = i;
-            x = (int) (400 + r * Math.cos(i * 0.1));
-            y = (int) (300 + r * Math.sin(i * 0.1));
-            g.drawLine(x, y, x, y);
+
+        /*
+            x = 0.5 * theta * cos(theta)
+            y = 0.5 * theta * sin(theta)
+            0 <= theta <= aumento
+        */
+
+        double theta = 0;
+        while (true) {
+            double x = 0.9 * theta * Math.cos(theta);
+            double y = 0.9 * theta * Math.sin(theta);
+
+            double x2 = 0.9 * (theta + 0.2) * Math.cos(theta + 0.2);
+            double y2 = 0.9 * (theta + 0.2) * Math.sin(theta + 0.2);
+
+            g.drawLine((int) (x + WIDTH/2), (int) (HEIGHT/2 - y), (int) (x2 + WIDTH/2), (int) (HEIGHT/2 - y2));
+
+            theta += 0.1;
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
-                System.out.println(e);
+                e.printStackTrace();
             }
         }
     }
@@ -38,7 +50,7 @@ class Arquimedes extends JFrame implements Runnable {
         repaint();
     }
 
-    public static void main(String[] args) {
+    void main() {
         Arquimedes arq = new Arquimedes();
         arq.setVisible(true);
         Thread t = new Thread(arq);
